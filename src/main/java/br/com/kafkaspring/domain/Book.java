01cefd10c11b4e42553fd4f41b5fdbb4aa720d66
Carlos <carlos.jeronimo@allin.com.br>
2022-02-24 21:37:37 -0300

    Add project to repository

---
+++ src/main/java/br/com/kafkaspring/domain/Book.java
@@ -0,0 +1,22 @@
+package br.com.kafkaspring.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @ManyToOne
+    private People people;
+
+}
